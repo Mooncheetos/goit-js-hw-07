d308debbd12b6db65f55b9140de4440d47aaa1a3
Mooncheetos <143450665+Mooncheetos@users.noreply.github.com>
2024-01-11 00:39:38 +0200

    deromo

--- js/task-2.js
+++ js/task-2.js
@@ -27,3 +27,30 @@ const images = [
     alt: 'Zebras on Zebra',
   },
 ];
+const galleryContainer = document.querySelector('.gallery');
+
+const createGalleryItem = ({ url, alt }) => {
+  const listItem = document.createElement('li');
+  listItem.classList.add('gallery-item');
+
+  const image = document.createElement('img');
+  image.src = url;
+  image.alt = alt;
+  image.classList.add('gallery-image');
+
+  listItem.appendChild(image);
+  return listItem;
+};
+
+const appendGalleryItems = (galleryItems) => {
+  const galleryFragment = document.createDocumentFragment();
+
+  galleryItems.forEach((imageData) => {
+    const galleryItem = createGalleryItem(imageData);
+    galleryFragment.appendChild(galleryItem);
+  });
+
+  galleryContainer.appendChild(galleryFragment);
+};
+appendGalleryItems(images);
+
